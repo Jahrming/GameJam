fd400222a63f2f8a7bf5e01cd2517e3857feb4a0
train66 <william.prada27@gmail.com>
2025-01-26 09:02:37 -0500

    9:02

--- scenes/GameOver.gd
+++ scenes/GameOver.gd
@@ -1,11 +1,31 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	Engine.time_scale = 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_texture_button_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	Engine.time_scale = 1
+
+
+func _on_texture_button_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	Engine.time_scale = 1
+
+
+func _on_texture_button_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	Engine.time_scale = 1
+
+
+func _on_texture_button_5_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	Engine.time_scale = 1
+
+
+func _on_quit_pressed():
+	get_tree().quit()
---
+++ scenes/Proximamente.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+
+func _on_atras_pressed():
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
