f8ee908947d608a567842e042624e98ec22aaf7e
train66 <william.prada27@gmail.com>
2025-01-26 04:49:16 -0500

    4:50

--- scenes/menupantallanv_1.gd
+++ scenes/menupantallanv_1.gd
@@ -1,9 +1,12 @@
 extends Control
 
+@onready var main = $".."
+
 
 func _on_return_pressed():
 	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+	Engine.time_scale = 1
 
 func _on_pause_pressed():
-	pass
+	main.pauseMenu()
 	
--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -1,14 +1,17 @@
 extends Control
 
-@onready var main = $"../../"
+@onready var main = $"../.."
 # Reanudar el juego
 func _on_atras_pressed():
 	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")  # Cambia a la escena del menú principal
+	Engine.time_scale = 1
 	
-func _on_reiniciar_pressed():
-	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
 	
 func _on_reanudar_pressed():
 	main.pauseMenu()
 	
 	
+
+func _on_reiniciar_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	Engine.time_scale = 1
